f43a7d8def3825d9b071a8fe348bbf38e0845a87
FabreFacundo <facufabre2011@gmail.com>
2023-10-12 08:50:39 -0300

    navBar fxd-top only scrlUp

--- scripts/script.js
+++ scripts/script.js
@@ -1,9 +1,36 @@
-var navBar=document.getElementById("barraNav");
-window.onresize=function(){
-    let anchoPantalla=window.innerWidth;
-    console.log(anchoPantalla)
+var navBar = document.getElementById("barraNav");
+var enCalculo=false;
+window.onload=function()
+{
+    window.setInterval(() => {
+        posicionInicial=window.scrollY;
+    }, 1);
+}
+window.onscroll = function () {
+    let anchoPantalla = window.innerWidth;
+    let posicionNueva = window.scrollY;
+    if (posicionNueva < (posicionInicial)) {
+        if (anchoPantalla > 800) {
+
+            if (!navBar.classList.contains("fixed-top"))
+                navBar.classList.add("fixed-top");
+        }
+        else {
+            if (navBar.classList.contains("fixed-top"))
+                navBar.classList.remove("fixed-top");
+        }
+    }
+    else {
+        if (navBar.classList.contains("fixed-top"))
+            navBar.classList.remove("fixed-top");
+    }
+
+}
+
+/*window.onresize=function(){
     if(anchoPantalla>800)
     {
+        
         if(!navBar.classList.contains("fixed-top"))
         navBar.classList.add("fixed-top");
     }
@@ -11,4 +38,4 @@ window.onresize=function(){
         if(navBar.classList.contains("fixed-top"))
         navBar.classList.remove("fixed-top");
     }
-}
+}*/
